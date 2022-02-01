d320ab12d038e957c6b02d4cc263896f9e8b4eac
Josiel Faria <developerjosiel@gmail.com>
2022-02-01 20:51:59 -0300

    Primerio commit com bs

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+
+
+const routes: Routes | any = [
+  { path: '', redirectTo: 'home', pathMatch: 'full', },
+  { path: 'home', component: HomeComponent, data: { routeName: 'Home' }, isMenu: true },
+  { path: '**', component: HomeComponent } // TODO: Criar PAGE NOT FOUND
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}

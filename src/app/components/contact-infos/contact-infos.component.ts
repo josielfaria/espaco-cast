c95efbe9b57062714cca53162dd77b1f4eddca2d
Josiel Faria <developerjosiel@gmail.com>
2022-02-11 01:16:47 -0300

    Alterando icons e teste

--- src/app/components/contact-infos/contact-infos.component.ts
+++ src/app/components/contact-infos/contact-infos.component.ts
@@ -9,15 +9,15 @@ import { ContactInfosModel } from 'src/app/models/contact-infos.model';
 export class ContactInfosComponent {
 
   infos: Array<ContactInfosModel> = [
-    { icon: 'map', text: 'QL7 Conjunto B, Lote 12, 58 - Riacho Fundo I, DF' },
-    { icon: 'tel', text: '(617) 557-0089' },
-    { icon: 'email', text: 'contact@example.com' }
+    { icon: 'icon-map', text: 'QL7 Conjunto B, Lote 12, 58 - Riacho Fundo I, DF' },
+    { icon: 'icon-tel', text: '(617) 557-0089' },
+    { icon: 'icon-email', text: 'contact@example.com' }
   ];
 
   constructor() { }
 
   getIconUrl(iconName: string): string {
-    return `../../../assets/icons/${iconName}.png`;
+    return `../../../assets/images/icons/${iconName}.svg`;
   }
 
 }
